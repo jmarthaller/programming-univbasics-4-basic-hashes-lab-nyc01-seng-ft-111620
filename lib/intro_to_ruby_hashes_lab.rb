461b8f374963756b1508c7c1ab06b1972266abbe
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-15 19:00:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  return puppies = { :name => "Bandit", :type => "Labradoodle", :mood => "friendly", :age => 2  }
 end
 
 def pioneer
